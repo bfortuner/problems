0bef1558c3efa11109f5bc5d1cf3ef957990edd8
bfortuner <bfortuner@gmail.com>
2014-12-23 09:37:20 -0800

    Completed 4 versions of isAnagram

--- string_manipulation/isAnagram.java
+++ string_manipulation/isAnagram.java
@@ -1,17 +1,35 @@
+import java.util.Arrays;
+
 public class isAnagram {
 
     public static void main(String[] args) {
         String in1 = "abcd";
         String in2 = "dcab";
         System.out.println(isAnagram(in1, in2));
+        System.out.println(isAnagramArray(in1, in2));
+        System.out.println(isAnagramBitwise(in1, in2));
+        System.out.println(isAnagramSort(in1, in2));
 
         String in3 = "acdbb";
         String in4 = "dcab";
         System.out.println(isAnagram(in3, in4));
+        System.out.println(isAnagramArray(in3, in4));
+        System.out.println(isAnagramBitwise(in3, in4));
+        System.out.println(isAnagramSort(in3, in4));
 
         String in5 = "acd";
         String in6 = "cada";
         System.out.println(isAnagram(in5, in6));
+        System.out.println(isAnagramArray(in5, in6));
+        System.out.println(isAnagramBitwise(in5, in6));
+        System.out.println(isAnagramSort(in5, in6));
+
+        String in7 = "aaabbbef";
+        String in8 = "bbbbeeef";
+        System.out.println(isAnagram(in7, in8));
+        System.out.println(isAnagramArray(in7, in8));
+        System.out.println(isAnagramBitwise(in7, in8));
+        System.out.println(isAnagramSort(in7, in8));
 
     }
 
@@ -40,5 +58,52 @@ public class isAnagram {
         }   
    }
 
+    public static boolean isAnagramArray(String str1, String str2) {
+        int[] arr = new int[256];
+        for (int i=0; i<str1.length(); i++) {
+            int ascii = (int) str1.charAt(i);
+            arr[ascii]++;
+        }
+        for (int j=0; j<str2.length(); j++) {
+            int ascii = (int) str2.charAt(j);
+            arr[ascii]--;
+        }
+        for (int k : arr) {
+            if (k != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAnagramBitwise(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int x = 0;
+        for (int i=0; i<str1.length(); i++) {
+            int a1 = (int) str1.charAt(i);
+            int a2 = (int) str2.charAt(i);
+            x = x ^ a1;
+            x = x ^ a2;
+        }
+        return x == 0;
+    }
+
+    public static boolean isAnagramSort(String str1, String str2) {
+        //convert String to array
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        //sort array
+        java.util.Arrays.sort(arr1);
+        java.util.Arrays.sort(arr2);
+
+        //convert back to String
+        str1 = new String(arr1);
+        str2 = new String(arr2);
+
+        return str1.equals(str2);
+    }
 
 }
