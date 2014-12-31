52758bb9e86f1b33ef09316014a0200c383815e5
bfortuner <bfortuner@gmail.com>
2014-12-31 07:53:47 -0800

    1) Added ChainingHashTable class and unit tests

--- hashtables/ChainingHashTable.java
+++ hashtables/ChainingHashTable.java
@@ -16,62 +16,48 @@ public class ChainingHashTable implements HashTable {
 
     public void put(String key, String val) {
         Pair p = new Pair(key, val);
-    	int bucket = getBucketFromKey(key);
-    	
+    	int bucket = getBucketFromKey(key);    	
         if (table[bucket] == null) {
             table[bucket] = p;
-        
         } else {
             Pair next = table[bucket];
-        
             while (next.getNext() != null) {
                 next = next.getNext();
             }
             next.setNext(p);
         }
-
     }
     
     public String get(String key) {
     	int bucket = getBucketFromKey(key);
-    	
         if (table[bucket] == null) {
             return null;
-        
         } else {    
             Pair tmp = table[bucket];
-        
             while (tmp.getKey() != key) {
                 tmp = tmp.getNext();
             }
-
             if (tmp == null) {
                 return null;
             } else {
                 return tmp.getValue();
             }
         }
-
     }
 
     public void remove(String key) {
     	int bucket = getBucketFromKey(key);
         Pair prior = table[bucket];
         Pair cur = table[bucket];
-        
         if (cur.getKey() == key) {
             table[bucket] = cur.getNext();
-        
         } else {
-        
             while (cur.getKey() != key) {
                 prior = cur;
                 cur = cur.getNext();
            }
            prior.setNext(cur.getNext());
-        
         }
-
     }
 
     public int getBucketFromKey(String key) {
