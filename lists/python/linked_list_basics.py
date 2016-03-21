87c3598ede34addfcdaa9018e090e57942aba558
Brendan Fortuner <fortuner@amazon.com>
2016-03-20 17:25:51 -0700

    added get_nth_node_from_tail python impl in linked list section

--- lists/python/linked_list_basics.py
+++ lists/python/linked_list_basics.py
@@ -17,6 +17,22 @@ def get_max_value_in_list_recursive(node):
 		return max(node.get_value(), get_max_value_in_list_recursive(
 			node.get_next_node()))
 
+def get_nth_node_from_tail(node, pos_from_tail):
+	"""
+	Get Node data of the Nth Node from the end.
+ 	head could be None as well for empty list
+	"""
+	# Use a queue to keek track of last pos_from_tail values, then dequeue at end
+	# Use list, then return list[len(list)-pos_from_tail]
+	# Two loops. Find length, then stop when you get to Node (if you can't use data structure)
+	if node is None:
+		return None
+	vals_list = []
+	while node is not None:
+		vals_list.append(node.value)
+		node = node.next_node
+	return vals_list[len(vals_list) - pos_from_tail - 1]
+
 
 
 # Tests
@@ -47,9 +63,16 @@ def test_get_max_value_in_list_recursive():
 	node = l1.get_first_node().get_next_node()
 	assert get_max_value_in_list_recursive(node) == 3
 
+def test_get_nth_node_from_tail():
+	l1 = build_ll_from_lst([1,2,3])
+	node = l1.get_first_node()
+	assert get_nth_node_from_tail(node,0) == 3
+	assert get_nth_node_from_tail(node,1) == 2
+	assert get_nth_node_from_tail(node,2) == 1
 
 if __name__ == "__main__":
 	print "Running linked_list_basic tests!"
 	test_get_max_value_in_list_iterative()
 	test_get_max_value_in_list_recursive()
+	test_get_nth_node_from_tail()
 	print "Completed linked_list_basic tests!"
