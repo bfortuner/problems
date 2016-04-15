dc2641b2fe71eec64eb403dcce2aa2cc9ffaef58
Fortuner <bfortuner@gmail.com>
2016-04-15 13:14:59 -0700

    added linear solution to the max 3 problem in python

--- arrays/python/max_sum_3.py
+++ arrays/python/max_sum_3.py
@@ -24,6 +24,17 @@ def max_sum_3_loops(arr):
 				max_sum = max(cur_sum, max_sum)
 	return max_sum
 
+def max_sum_3_linear(arr):
+	max_elems = sorted(arr[:3])
+	for num in arr[3:]:
+		if num > max_elems[0]:
+			max_elems[0] = num
+		elif num > max_elems[1]:
+			max_elems[1] = num
+		elif num > max_elems[2]:
+			max_elems[2] = num
+	return sum(max_elems)
+
 
 
 
@@ -41,7 +52,11 @@ def test_max_sum_3_loops():
 	assert max_sum_3_loops(a1) == 9
 	assert max_sum_3_loops(a2) == 19
 
+def test_max_sum_3_linear():
+	assert max_sum_3_linear(a1) == 9
+	assert max_sum_3_linear(a2) == 19
 
 if __name__ == "__main__":
 	test_max_sum_3_sort()
 	test_max_sum_3_loops()
+	test_max_sum_3_linear()
