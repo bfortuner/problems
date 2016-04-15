57ae8c2027a5704d544bf8c83e677fa1e9978ed4
Fortuner <bfortuner@gmail.com>
2016-04-15 14:03:46 -0700

    added recurisve binary search solution in python

--- searching/python/binary_search.py
+++ searching/python/binary_search.py
@@ -19,10 +19,24 @@ def binary_search(arr, val):
 	return False
 
 
+def binary_search_recur(arr, val):
+	if arr is None or len(arr) == 0:
+		return False
+	mid = len(arr)/2
+	if arr[mid] == val:
+		return True
+	elif arr[mid] > val:
+		return binary_search_recur(arr[:mid], val)
+	else:
+		return binary_search_recur(arr[mid+1:], val)
+
+
+
 
 # Tests
 a1 = [1,2,3,4,5]
 a2 = [1,2]
+a3 = [1]
 
 def test_binary_search():
 	assert binary_search(a1, 2) == True
@@ -35,8 +49,26 @@ def test_binary_search():
 	assert binary_search(a2, 2) == True
 	assert binary_search(a2, -3) == False
 
+	assert binary_search(a3, 1) == True
+	assert binary_search(a3, 0) == False
+	assert binary_search(a3, 3) == False
 
+def test_binary_search_recur():
+	assert binary_search_recur(a1, 2) == True
+	assert binary_search_recur(a1, 9) == False
+	assert binary_search_recur(a1, 1) == True
+	assert binary_search_recur(a1, 0) == False
+	
+	assert binary_search_recur(a2, 5) == False
+	assert binary_search_recur(a2, 1) == True
+	assert binary_search_recur(a2, 2) == True
+	assert binary_search_recur(a2, -3) == False
+
+	assert binary_search_recur(a3, 1) == True
+	assert binary_search_recur(a3, 0) == False
+	assert binary_search_recur(a3, 3) == False
 
 if __name__ == "__main__":
 	test_binary_search()
+	test_binary_search_recur()
 
